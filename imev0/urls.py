1299557ab57e951c23dd7f3a6b18432ed225f749
Milad <milad.ameri73@gmail.com>
2015-12-18 16:02:49 +0330

    server works correctly now and we can send and receive datas

---
+++ imev0/urls.py
@@ -0,0 +1,10 @@
+__author__ = 'MiladDK'
+
+__author__ = 'MiladDK'
+from django.conf.urls import url
+from .views import Index,Datas
+
+urlpatterns = [
+    url(r'^$', Index.as_view(), name='index'),
+    url(r'^datas$', Datas.as_view(), name='getDatas'),
+]
